2b2251637f86873b097645548f697a873db1961f
matteoetzer@gmx.at <matteoetzer@gmx.at>
2019-12-11 11:20:39 +0100

    script calculator, resets fields not intended for use

---
+++ Assets/Scripts/ComputeNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public Text result;
+    public InputField ipfVarA;
+    public InputField ipfVarB;
+    public Button btnSubNumbers;
+    public Button btnReset;
+
+    int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetOnClick(){
+    ipfVarA.interactable = true;
+    ipfVarB.interactable = true;
+    btnSubNumbers.interactable = true;
+    btnReset.interactable = false;
+    result.text = "Result";
+    ipfVarA.text = "";
+    ipfVarB.text = "";
+    //result.text = 0;
+    }
+    public void SubNumbersOnClick(){
+        int variableA = int.Parse(ipfVarA.text);
+        int variableB = int.Parse(ipfVarB.text);
+        int SubResult = variableA - variableB;
+        result.text = SubResult.ToString();
+        ipfVarA.interactable = false;
+        ipfVarB.interactable = false;
+        btnSubNumbers.interactable = false;
+        btnReset.interactable = true;
+    }
+
+
+
+}
