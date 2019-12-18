202b20bbbef9cb30e2a83d920f3afcb2122bec22
matteoetzer@gmx.at <matteoetzer@gmx.at>
2019-12-18 10:42:26 +0100

    pythagoras finished

---
+++ Assets/Scripts/hoch2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoch2 : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    public int value;
+    public float a;
+    public float b;
+
+    int Square ( int a)
+    {
+        //Debug.Log(a * a);
+        return (a * a);
+    }
+    
+    void Start()
+    {
+        int result = Square(value) + Square(value);
+        //Debug.Log("result Addition: " + result);        
+        Pythagoras(a,b);
+
+    }
+
+    void OnValidate ()
+    {
+        //Square(value);  
+
+    }
+    // Update is called once per frame
+    void Pythagoras(float a, float b)
+    {
+            float c = Mathf.Sqrt(a*a + b*b);
+            Debug.Log("c: " + c);
+    }
+        
+}
